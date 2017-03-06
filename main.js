ea96eb826958c86b92424c85aeea9ba4f3a97304
Deepanjan Roy <dproy@google.com>
2017-03-06 09:08:24 -0500

    Tab key handling in textarea

--- main.js
+++ main.js
@@ -62,3 +62,29 @@ window.addEventListener("beforeunload", function (e) {
     e.returnValue = "No!";
 });
 
+
+// Handle tab in text area
+document.querySelector("textarea").addEventListener('keydown',function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var target = e.target;
+        var value = target.value;
+
+        // set textarea value to: text before caret + tab + text after caret
+        target.value = value.substring(0, start)
+            + "  "
+            + value.substring(end);
+
+        // put caret at right position again (add two spaces)
+        this.selectionStart = this.selectionEnd = start + 2;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+},false);
+
+
+// TODO: Make the return key put add equal amounts of indent as previous line
